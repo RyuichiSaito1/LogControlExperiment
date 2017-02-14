b25d6ff44702b2e93471f0cdfc2566a09e680d1a
Ryuichi Saito <Ryuichi@Ryuichis-MacBook-Air.local>
2017-02-14 17:50:45 +0900

    Add comment.

--- src/main/scala-2.11/jp/ac/keio/sdm/SimpleApp.scala
+++ src/main/scala-2.11/jp/ac/keio/sdm/SimpleApp.scala
@@ -17,61 +17,72 @@ object SimpleApp {
 
   def main(args: Array[String]): Unit = {
 
+    /** Load Log4j properties and Set log level */
     val props = new Properties()
     props.load(getClass.getClassLoader.getResourceAsStream("log4j.properties"))
     PropertyConfigurator.configure(props)
     val log = LogManager.getRootLogger()
     log.setLevel(Level.WARN)
 
+    /** Create a Spark Streaming */
     val conf = new SparkConf().setMaster("local[*]").setAppName("Simple Application")
     val ssc = new StreamingContext(conf, Durations.minutes(1L))
+
+    /** Create a input stream that returns tweets received from Twitter */
     val filter = if (args.isEmpty) Nil else args.toList
     val stream = TwitterUtils.createStream(ssc, None, filter)
 
+    /** Create a Log Cache */
     val lc = new LogCache()
+
+    /** Create a Log Filter that periodically output logs from a Log Cache */
     val lf = new LogFilter(lc)
 
-    try {
-      stream
-        .flatMap { status =>
-          val text = status.getText
+    stream
 
-          val analyzer = new JapaneseAnalyzer
-          val tokenStream = analyzer.tokenStream("", text)
-          val charAttr = tokenStream.addAttribute(classOf[CharTermAttribute])
+      .flatMap { status =>
+        val text = status.getText
+        // Create a Analyzer
+        val analyzer = new JapaneseAnalyzer
+        // Returns a TokenStream suitable for fieldName, tokenizing the contents of reader
+        val tokenStream = analyzer.tokenStream("", text)
+        // This method first checks if an instance of that class is already in this AttributeSource and returns it
+        // Otherwise a new instance is created, added to this AttributeSource and returned
+        val charAttr = tokenStream.addAttribute(classOf[CharTermAttribute])
+        // This method is called by a consumer before it begins consumption using incrementToken()
+        tokenStream.reset()
 
-          tokenStream.reset()
-
-          try {
-            Iterator
-              .continually(tokenStream.incrementToken())
-              .takeWhile(identity)
-              .map(_ => charAttr.toString)
-              .toVector
-          } finally {
-            tokenStream.end()
-            tokenStream.close()
-          }
+        try {
+          Iterator
+            // Creates an infinite-length iterator returning the results of evaluating an expression.
+            // Advance the stream to the next token
+            .continually(tokenStream.incrementToken())
+            // An iterator returning elements from it as long as condition p is true
+            .takeWhile(identity)
+            .map(_ => charAttr.toString)
+            // Convert this vector to Vector
+            .toVector
+        } finally {
+          tokenStream.end()
+          tokenStream.close()
         }
-        .flatMap(_ split " ")
-        .map(s => { try {
-          s(10000)
-        } catch {
-          case runtime : RuntimeException => {
-            lc.putIfAbsent(Thread.currentThread().getId + "MessageId", runtime.toString())
-            // Need "import scala.collection.JavaConversions._" to convert Java API to Scala API.
-            lc.foreach(kv => println(kv._1 + " -> " + kv._2))
-          }
-        }
-        })
-        .map(word => (word, 1))
-        .reduceByKey((a, b) => a + b)
-        .saveAsTextFiles("output/tweet")
-    } catch {
-      case e: StringIndexOutOfBoundsException => {
-        log.warn("IndexOutOfBoundsException")
       }
-    }
+
+      .flatMap(_ split " ")
+      .map(s => { try {
+        s(10000)
+      } catch {
+        case runtime : RuntimeException => {
+          lc.putIfAbsent(Thread.currentThread().getId + "MessageId", runtime.toString())
+          // Need "import scala.collection.JavaConversions._" to convert Java API to Scala API.
+          lc.foreach(kv => println(kv._1 + " -> " + kv._2))
+        }
+      }
+      })
+      .map(word => (word, 1))
+      .reduceByKey((a, b) => a + b)
+      .saveAsTextFiles("output/tweet")
+
     ssc.start()
     log.warn("Spark Streaming Start")
     ssc.awaitTermination()
