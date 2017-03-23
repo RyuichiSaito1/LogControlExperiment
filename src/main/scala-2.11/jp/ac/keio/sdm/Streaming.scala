34eaf9ad5925d1d5e511059ebb56b8afdf2475d5
Ryuichi Saito <ryuichi.saito@keio.jp>
2017-03-23 13:37:20 +0900

    Test the method level exception handling.

---
+++ src/main/scala-2.11/jp/ac/keio/sdm/Streaming.scala
@@ -0,0 +1,65 @@
+package jp.ac.keio.sdm
+
+import java.util.Properties
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{StreamingContext, Seconds}
+import org.apache.spark.SparkContext
+
+import org.apache.spark.streaming.twitter.TwitterUtils
+import org.apache.log4j.{Level, LogManager, Logger, PropertyConfigurator}
+import org.apache.lucene.analysis.ja.JapaneseAnalyzer
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute
+
+
+/**
+  * Created by Ryuichi on 3/23/2017 AD.
+  */
+object Streaming {
+
+  def doStreaming(args: Array[String], ssc: StreamingContext): Unit = {
+
+    /** Create a input stream that returns tweets received from Twitter */
+    val filter = if (args.isEmpty) Nil else args.toList
+    val stream = TwitterUtils.createStream(ssc, None, filter)
+
+    /** Create a Log Filter that periodically output logs from a Log Cache */
+    val lf = new LogFilter
+
+    stream
+
+      .flatMap { status =>
+        val text = status.getText
+        // Create a Analyzer
+        val analyzer = new JapaneseAnalyzer
+        // Returns a TokenStream suitable for fieldName, tokenizing the contents of reader
+        val tokenStream = analyzer.tokenStream("", text)
+        // This method first checks if an instance of that class is already in this AttributeSource and returns it
+        // Otherwise a new instance is created, added to this AttributeSource and returned
+        val charAttr = tokenStream.addAttribute(classOf[CharTermAttribute])
+        // This method is called by a consumer before it begins consumption using incrementToken()
+        tokenStream.reset()
+
+        try {
+          Iterator
+            // Creates an infinite-length iterator returning the results of evaluating an expression.
+            // Advance the stream to the next token
+            .continually(tokenStream.incrementToken())
+            // An iterator returning elements from it as long as condition p is true
+            .takeWhile(identity)
+            .map(_ => charAttr.toString)
+            // Convert this vector to Vector
+            .toVector
+        } finally {
+          tokenStream.end()
+          tokenStream.close()
+        }
+      }
+      .flatMap(_.split(" "))
+      .map(s => s(10000))
+      .map(word => (word, 1))
+      .reduceByKey((a, b) => a + b)
+      .saveAsTextFiles("output/tweet")
+      // .saveAsTextFiles("s3://aws-logs-757020086170-us-west-2/output/tweet")
+  }
+}
